01a00013208eb1e07d720cc1d8b53f5cd8bf56b7
its-gayeon <gayeonl0007@gmail.com>
2024-11-23 03:33:05 +0900

    ⚡Modified old_extract_motivation function to be more generous

--- extract_helper.py
+++ extract_helper.py
@@ -3,25 +3,28 @@ import pandas as pd
 
 # 보고서 parsing 관련
 def extract_section(content, start, end):
-    if isinstance(content, str):
-        match = re.search(rf'{re.escape(start)}(.*?){re.escape(end)}', content, re.DOTALL)
-        if match:
-            return match.group(1).strip()
-    return None
+    match = re.search(rf'{re.escape(start)}(.*?){re.escape(end)}', content, re.DOTALL)
+    if match:
+        return match.group(1).strip()
 
 def old_extract_motivation(content):
-    # 두 부분 추출
-    match1 = extract_section(content, "교환학생에 관심 갖게 된 계기 및 지원 동기", "출국 전 준비 사항과 주의 할 점")
-    match2 = extract_section(content, "교환학생 프로그램을 통해 느낀 점, 배운 점", "기타\(사진, 건의사항, 등\)")
+    if isinstance(content, str):
+        # 두 부분 추출
+        match1 = extract_section(content, "교환학생에 관심 갖게 된 계기 및 지원 동기", "출국 전 준비 사항과 주의 할 점")
+        match2 = extract_section(content, "교환학생 프로그램을 통해 느낀 점, 배운 점", "기타\(사진, 건의사항, 등\)")
 
-    # 결과 조합
-    result = ""
-    if match1:
-        result += match1
-    if match2:
-        result += match2
+        # 결과 조합
+        result = ""
+        if match1:
+            result += match1
+        if match2:
+            result += match2
 
-    return result.strip()  # 결과를 공백으로 조합하여 반환
+        if not match1 and not match2:
+            return content.strip()
+
+        return result.strip()  # 결과를 공백으로 조합하여 반환
+    return None
 
 def new_extract_motivation(content):
     # 세 부분 추출
